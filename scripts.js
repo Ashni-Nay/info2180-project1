79368289f972475abfaa90e039863486b1fb73ec
Ashni-Nay Wilson <ashninaywilson@yahoo.com>
2020-11-01 22:33:08 -0500

    Edited js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 /* Add your JavaScript to this file */
 
+window.onload=sendMsg;
+
 function sendMsg(){
-	var subscribe = document.getElementsByTagName("button")[0];
-	var pattern = /^w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-	subscribe.addEventListener("click", function(e){
+	var subbutt = document.getElementsByTagName("button")[0];
+	var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	subbutt.addEventListener("click", function(e){
 		var email = document.getElementById("email").value;
 		e.preventDefault();
 		if(email.match(pattern)){
-			document.getElementsByClassName("message")[0].innerHTML = ("Thank you! Your email address ${email} has been added to our mailing list!");
+			document.getElementsByClassName("message")[0].innerHTML = (`Thank you! Your email address ${email} has been added to our mailing list!`);
 		}
 		else{
 			document.getElementsByClassName("message")[0].innerHTML = ("Please enter a valid email address.");
@@ -15,5 +17,3 @@ function sendMsg(){
 	});
 }
 
-
-window.onload=sendMsg;
